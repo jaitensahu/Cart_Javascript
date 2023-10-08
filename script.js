c7bef1a1ccf25fe442c001f1d62f38ea9df35b75
Jaiten <Jaitensahu9@gmail.com>
2023-10-08 19:41:55 +0530

    updated

--- script.js
+++ script.js
@@ -25,12 +25,12 @@ product_list.forEach((val) => {
     Products.forEach((idx) => {
       if (items.innerText == 1 && parseInt(product_id) == idx.id) {
         addToCart(idx.id, idx.name, idx.price, items.innerText);
-        totalSum(idx.price);
+        totalSum(idx.price,items.innerText);
       } //if there is atleast no Of product is 1
       else if (parseInt(product_id) == idx.id) {
         let td2 = document.querySelector(`#data${idx.id}`);
         td2.innerText = `${items.innerText} x ${idx.price}`;
-        totalSum(idx.price);
+        totalSum(idx.price,items.innerText);
       }
       
     });
@@ -55,7 +55,7 @@ product_list.forEach((val) => {
         subtract(idx.price);
         cartItems--;
         superscript.innerText=`${cartItems}`;
-        console.log(cartItems);
+   
       } else if (parseInt(product_id) == idx.id) {
         let td2 = document.querySelector(`#data${idx.id}`);
         td2.innerText = `${items.innerText} x ${idx.price}`;
@@ -81,12 +81,14 @@ function addToCart(id, product_name, product_price, no_of_items) {
   table_data.innerText = `${product_name}`;
   table_data2.innerText = `${no_of_items} x ${product_price}`;
   table_data2.setAttribute("id", `data${id}`);
+  console.log("row=" ,row);
   row.appendChild(table_data);
+  console.log("row=" ,row);
   table_body.appendChild(row);
   row.appendChild(table_data2);
+  console.log("row=" ,row);
   row.style.borderBottom = "2px dashed black";
   table_body.appendChild(row);
-  // console.log(cartItems);
   superscript.innerText=`${cartItems}`;
 }
 
@@ -95,33 +97,26 @@ function removeFromCart(id, product_name, product_price, no_of_items) {
   let deleteRow = document.querySelector(`#row1-${id}`);
   table_body.removeChild(deleteRow);
 }
-// let multiplication = 0;
-// function calculate(price) {
-//   multiplication = parseInt(price);
-//   totalSum(multiplication);
-// }
 let cartItems=0;
 let totalPrice = document.querySelector("#totalPrice");
-function totalSum(price) {
+function totalSum(price,totalItem) {
+  if(totalItem<10){
   total += price;
   totalPrice.innerText = `${total}`;
-  console.log(cartItems);
 }
-function subtract(price) {
+}
+function subtract(price ) {
   total -= price;
-  // let totalPrice = document.querySelector("#totalPrice");
   totalPrice.innerText = `${total}`;
-  // console.log(cartItems);
 }
+
 
 let cartItem=document.getElementById("cartItem")
 let cart=document.querySelector(".cart");
 cart.addEventListener("click",()=>{
-  // console.log("clicked");
   cartItem.style.display="block"
 })
 let close=document.querySelector(".close");
 close.addEventListener("click",()=>{
-  // console.log("clicked");
   cartItem.style.display="none"
 })
